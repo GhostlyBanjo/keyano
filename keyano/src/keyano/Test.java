0e80fd5da9784aff4b6b1c8af0955a4dc63bebee
Alex Gleason <gleasonator@gmail.com>
2013-10-24 20:40:39 -0500

    Included JFugue, updated test file

--- keyano/src/keyano/Test.java
+++ keyano/src/keyano/Test.java
@@ -1,12 +1,44 @@
 package keyano;
 
-public class Test {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import org.jfugue.*;
+
+public class Test implements KeyListener {
+
+	private static Player player;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		player = new Player();
+		
+		//Works!
+		player.play(new Pattern("C D E F G A B"));
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//Doesn't work :(
+		switch (e.getKeyCode()) {
+		case KeyEvent.VK_A:
+			player.play(new Pattern("B"));
+			break;
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
